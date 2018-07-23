ee100b25d69f808ed82b7390c1597a799f4eaed6
gangjung <cru6548@gmail.com>
2018-07-24 05:36:15 +0900

    Upload Project Files

---
+++ Project/Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class CameraMove : MonoBehaviour {
+
+    private GameObject _player;
+    [Range(-500,500)]
+    public float x;
+    [Range(-500, 500)]
+    public float y;
+    [Range(-500, 500)]
+    public float z;
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player");
+
+        x = _player.transform.position.x;
+        y = _player.transform.position.y + 4;
+        z = _player.transform.position.z - 5;
+
+        transform.position = new Vector3(x, y, z);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = _player.transform.position + new Vector3(0, 4, -5);
+       // transform.position = Vector3.Lerp(transform.position, _player.transform.position + new Vector3(0, 4, -5), 0.1f);
+    }
+}
---
+++ Project/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    
+    private float _movespeed;
+    private GameObject _mainCamera;
+
+    private Vector3 before;
+
+    private bool asdf;
+
+    private void Awake()
+    {
+        _movespeed = 5;
+        _mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+	}
+
+    private void PlayerMove()
+    {
+        float _moveHori = Input.GetAxisRaw("Horizontal");
+        float _moveVerti = Input.GetAxisRaw("Vertical");
+
+        if (_moveHori == 0 && _moveVerti == 0)
+            return;
+
+        before = transform.position;
+
+        Vector3 movement = new Vector3(_moveHori, 0, _moveVerti) * Time.deltaTime * _movespeed;
+        transform.position += movement;
+
+        Quaternion quaternion = Quaternion.LookRotation(movement);
+        transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, 0.5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("tlfgod");
+            transform.position = before;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("탈출");
+        }
+    }
+}
