f2fd9e1489f7a5f0120456bd5381de09d2be9b82
gangjung <cru6548@gmail.com>
2018-08-17 23:01:21 +0900

    Login_Server

---
+++ Login_Server/Login_Server/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Login_Server
+{
+    class Player
+    {
+        TcpClient tc;
+        NetworkStream stream;
+
+        Socket asdf;
+
+        public Player(TcpClient tc, NetworkStream stream)
+        {
+            this.tc = tc;
+            this.stream = stream;
+
+            
+        }
+
+        public bool Read()
+        {
+            return true;
+        }
+    }
+}
--- Login_Server/Login_Server/Program.cs
+++ Login_Server/Login_Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Login_Server
 {
@@ -10,6 +12,33 @@ namespace Login_Server
     {
         static void Main(string[] args)
         {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+
+            byte[] buff = new byte[1024];
+
+            while (true)
+            {
+                Console.WriteLine("Connect");
+
+                TcpClient tc = listener.AcceptTcpClient();
+                NetworkStream stream = tc.GetStream();
+
+                int a; 
+
+                while ((a = stream.Read(buff, 0, buff.Length)) > 0)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(buff.ToString());
+                    stream.Write(buff, 0, buff.Length);
+                }
+                Console.WriteLine(a);
+
+                Console.WriteLine("Connect Finish");
+
+                stream.Close();
+                tc.Close();
+            }
         }
     }
 }
